2d6a6cb8689ee8e13d158e197860a5d178525262
c-rick <caizerui@idea.edu.cn>
2022-06-06 15:40:49 +0800

    feat: address add addressToShard fn

--- src/address.ts
+++ src/address.ts
@@ -35,3 +35,28 @@ export function isDioxideAddress(address: string, alg: 'ed25519'): boolean {
     return false
   }
 }
+
+/**
+ * address to shard
+ * @param address
+ * @param shardOrder
+ * @returns shard
+ */
+
+ export const addressToShard = (address: string, shardOrder: number): number => {
+  const colonIdx = address.lastIndexOf(':')
+  if (colonIdx == -1) throw 'invalid address format'
+  const unit8Array = new Uint8Array(base32Decode(address.substring(0, colonIdx), 'Crockford'))
+  const decoded = String.fromCharCode.apply(null, [...unit8Array])
+  if (!decoded || decoded.length != 36) throw 'invalid address format'
+  const dwords = new Uint32Array(9)
+  for (let i = 0; i < 9; i++) {
+    dwords[i] = 0
+    for (let j = 0; j < 4; j++) {
+      dwords[i] += decoded.charCodeAt(i * 4 + j) << (j * 8)
+    }
+  }
+  const shardDword = dwords[0] ^ dwords[7] ^ dwords[4]
+  const shardMask = ~(0xffffffff << shardOrder)
+  return shardDword & shardMask
+}
